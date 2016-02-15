6b2896a182b8d98a374ae99cb009f6f4e8ed50d0
Tang <weitang114@gmail.com>
2016-02-15 18:30:52 +0800

    sort result filenames

--- retrievis.py
+++ retrievis.py
@@ -4,6 +4,7 @@ from flask import request, render_template, flash, redirect, \
     url_for, Blueprint, g, Flask
 from glob import glob
 import os
+import re
 
 assert len(argv)==2, 'Usage: python retrievis.py <result dir>' 
 
@@ -14,9 +15,25 @@ app.config['RESULT_DIR'] = argv[1]
 @app.route('/')
 def index():
     res_paths = glob(app.config['RESULT_DIR'] + '/*.txt')
-    res_files = [os.path.basename(f) for f in res_paths]
+    res_files = sorted([os.path.basename(f) for f in res_paths], key=_get_sortvalue)
     return render_template('index.html', res_files=res_files)
  
+
+## return keys of filenames:
+##   1. prefix
+##   2. number 1
+##   3. number 2 ...
+##  eg. result_1.txt is before  result_2.txt  (1 < 2)
+##      abc.txt      is before  result.txt    (a < r)
+##      res_1_3.txt  is before  res_1_5.txt   (3 < 5)
+def _get_sortkey(filename):
+    numbers = [int(s) for s in re.findall('\d+', filename)]
+
+    # all chars before any number in filename
+    prefix = re.match('^((?![0-9]).)*', filename).group(0)
+
+    return [prefix] + numbers
+
 @app.route('/results/image')
 @app.route('/image')
 def get_image():
